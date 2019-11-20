3e22731e7a3ad14679f0db0ca2f8fee3f1b80bd2
BeingBright <BrightonvandenEnd@gmail.com>
2019-11-20 13:50:31 +0100

    Added .gitignore and cleaned some classes

--- src/test/java/nl/quintor/qodingchallenge/QodingchallengeApplicationTests.java
+++ src/test/java/nl/quintor/qodingchallenge/QodingchallengeApplicationTests.java
@@ -1,5 +1,6 @@
 package nl.quintor.qodingchallenge;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class QodingchallengeApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
