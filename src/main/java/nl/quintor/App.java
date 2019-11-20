7ab454da53d3dd8acbf383449fad2f02e134c277
Nick <nick.braks@live.nl>
2019-11-20 10:58:45 +0100

    Initial Commit

---
+++ src/main/java/nl/quintor/App.java
@@ -0,0 +1,13 @@
+package nl.quintor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
