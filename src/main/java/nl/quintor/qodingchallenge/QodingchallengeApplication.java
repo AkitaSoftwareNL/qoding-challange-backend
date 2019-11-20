02222d6e2ece19771f23457bb4420825ad301223
Nick <nick.braks@live.nl>
2019-11-20 11:22:50 +0100

    Initial commit

---
+++ src/main/java/nl/quintor/qodingchallenge/QodingchallengeApplication.java
@@ -0,0 +1,13 @@
+package nl.quintor.qodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodingchallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QodingchallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/quintor/qodingchallenge/QodingchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.quintor.qodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QodingchallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
