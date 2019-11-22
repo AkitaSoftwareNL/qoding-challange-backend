18c22fd6defa4d3148fc96f58ac549754464c017
Bas-Martin <48922519+Bas-Martin@users.noreply.github.com>
2019-11-22 13:05:47 +0100

    Check of dit merge conflict oplost

--- src/main/java/nl/quintor/qodingchallenge/rest/QuestionResource.java
+++ src/main/java/nl/quintor/qodingchallenge/rest/QuestionResource.java
@@ -1,23 +1,10 @@
 package nl.quintor.qodingchallenge.rest;
 
-import nl.quintor.qodingchallenge.dto.GivenAnswerDTO;
-import nl.quintor.qodingchallenge.service.QuestionService;
-import nl.quintor.qodingchallenge.service.QuestionServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-public class QuestionResource {
-
-    private QuestionService questionService = new QuestionServiceImpl();
-
-    @PostMapping("/{campaignName}/{participantID/{questionID}")
-    @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity answerQuestion(@RequestParam GivenAnswerDTO givenAnswer) {
-
-        questionService.validateAnswer(givenAnswer);
 
         return new ResponseEntity(HttpStatus.OK);
     }
