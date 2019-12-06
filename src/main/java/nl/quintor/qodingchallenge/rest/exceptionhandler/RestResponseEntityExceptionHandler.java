40e5457cbbb1a18304fb4b166184f4b9c84ff978
jeroen <jerrik1998@gmail.com>
2019-12-06 13:19:40 +0100

    Fixed problem

--- src/main/java/nl/quintor/qodingchallenge/rest/exceptionhandler/RestResponseEntityExceptionHandler.java
+++ src/main/java/nl/quintor/qodingchallenge/rest/exceptionhandler/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package nl.quintor.qodingchallenge.rest.exceptionhandler;
 
 import nl.quintor.qodingchallenge.persistence.exception.AnswerNotFoundException;
+import nl.quintor.qodingchallenge.persistence.exception.NoQuestionFoundException;
 import nl.quintor.qodingchallenge.service.exception.CampaignAlreadyExistsException;
 import nl.quintor.qodingchallenge.service.exception.NoCampaignFoundException;
 import org.slf4j.Logger;
@@ -26,7 +27,7 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         return new ResponseEntity<>("An exception has occured with the database", new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler({SQLException.class})
+    @ExceptionHandler({NoQuestionFoundException.class})
     public ResponseEntity<Object> handleNoQuestionFoundException(Exception e, WebRequest request) {
         logger.error(e.fillInStackTrace().toString());
         return new ResponseEntity<>("Question could not be found", new HttpHeaders(), HttpStatus.NOT_FOUND);
