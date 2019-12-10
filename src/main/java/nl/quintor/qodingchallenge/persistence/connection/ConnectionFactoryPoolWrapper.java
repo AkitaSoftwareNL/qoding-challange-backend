18f9a3ee585928df634c3420f307face39d0b031
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2019-12-10 14:42:54 +0100

    Hotfix questions

--- src/main/java/nl/quintor/qodingchallenge/persistence/connection/ConnectionFactoryPoolWrapper.java
+++ src/main/java/nl/quintor/qodingchallenge/persistence/connection/ConnectionFactoryPoolWrapper.java
@@ -2,7 +2,6 @@ package nl.quintor.qodingchallenge.persistence.connection;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionFactoryPoolWrapper {
     public static final ConnectionFactoryPoolWrapper wrapper = new ConnectionFactoryPoolWrapper();
