8c02ee5360eadf6d15ab380bfe85342bc962d465
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2019-12-19 15:05:12 +0100

    Fixed bas

--- src/main/java/nl/quintor/qodingchallenge/persistence/dao/ReportDAOImpl.java
+++ src/main/java/nl/quintor/qodingchallenge/persistence/dao/ReportDAOImpl.java
@@ -22,7 +22,7 @@ public class ReportDAOImpl implements ReportDAO {
                 Connection connection = getConnection()
         ) {
             PreparedStatement statement = connection.prepareStatement(
-                    "SELECT ga.GIVEN_ANSWER, q.QUESTION, ga.STATE, q.QUESTION_TYPE FROM given_answer as ga INNER JOIN question q " +
+                    "SELECT ga.GIVEN_ANSWER, q.QUESTION, ga.STATEID, q.QUESTION_TYPE FROM given_answer as ga INNER JOIN question q " +
                             "ON ga.QUESTIONID = q.QUESTIONID INNER JOIN conference as c " +
                             "ON ga.PARTICIPANTID = c.PARTICIPANTID WHERE ga.CAMPAIGN_ID = ? AND ga.PARTICIPANTID = ?");
             statement.setInt(1, campaignID);
