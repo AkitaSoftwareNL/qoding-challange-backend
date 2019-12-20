8e062bb6a5e859e3080d3eff052737e116424908
Bas-Martin <48922519+Bas-Martin@users.noreply.github.com>
2019-12-20 12:25:14 +0100

    Revert "ranked participantquery fixed?"
    This reverts commit 7b21c587

--- src/main/java/nl/quintor/qodingchallenge/persistence/dao/ReportDAOImpl.java
+++ src/main/java/nl/quintor/qodingchallenge/persistence/dao/ReportDAOImpl.java
@@ -1,7 +1,6 @@
 package nl.quintor.qodingchallenge.persistence.dao;
 
 import nl.quintor.qodingchallenge.dto.AnswerDTO;
-import nl.quintor.qodingchallenge.service.QuestionState;
 import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
@@ -23,12 +22,11 @@ public class ReportDAOImpl implements ReportDAO {
                 Connection connection = getConnection()
         ) {
             PreparedStatement statement = connection.prepareStatement(
-                    "SELECT poc.PARTICIPANTID, poc.CAMPAIGN_ID, poc.TIME_SPEND, c.FIRSTNAME, c.INSERTION, c.LASTNAME, c.EMAIL, c.PHONENUMBER, \n" +
-                            "(SELECT COUNT(*) FROM given_answer AS ga WHERE CAMPAIGN_ID = ? AND ga.PARTICIPANTID = poc.PARTICIPANTID AND STATEID = ?) AS CORRECT\n" +
-                            "FROM participant_of_campaign AS poc inner join conference as c ON poc.PARTICIPANTID = c.PARTICIPANTID WHERE CAMPAIGN_ID = ? ORDER BY CORRECT DESC, TIME_SPEND");
+                    "SELECT ga.GIVEN_ANSWER, q.QUESTION, ga.STATEID, q.QUESTION_TYPE FROM given_answer as ga INNER JOIN question q " +
+                            "ON ga.QUESTIONID = q.QUESTIONID INNER JOIN conference as c " +
+                            "ON ga.PARTICIPANTID = c.PARTICIPANTID WHERE ga.CAMPAIGN_ID = ? AND ga.PARTICIPANTID = ?");
             statement.setInt(1, campaignID);
-            statement.setInt(2, QuestionState.CORRECT.getState());
-            statement.setInt(3, campaignID);
+            statement.setString(2, participantID);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 answers.add(
