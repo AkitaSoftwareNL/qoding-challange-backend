1dc8c7a8e97a22d5c7e9e7215b29aec2e84d481a
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2020-01-08 14:38:39 +0100

    Added javadoc

--- src/main/java/nl/quintor/qodingchallenge/util/HashMapUtils.java
+++ src/main/java/nl/quintor/qodingchallenge/util/HashMapUtils.java
@@ -10,6 +10,13 @@ public class HashMapUtils {
     private HashMapUtils() {
     }
 
+    /**
+     * Checks if the key already exists in an HashMap
+     *
+     * @param keyExtractor Object which contains the key
+     * @param <T>          Method call to get the key
+     * @return true if the key exists in the HashMap
+     */
     public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
         Map<Object, Boolean> seen = new ConcurrentHashMap<>();
         return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
