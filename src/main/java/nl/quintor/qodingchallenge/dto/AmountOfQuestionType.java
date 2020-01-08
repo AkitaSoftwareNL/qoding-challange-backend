f5826dafd577103061b1a8272ee96655b006ffbe
BeingBright <BrightonvandenEnd@gmail.com>
2020-01-08 13:27:40 +0100

    Create AmountOfQuestionType.java

---
+++ src/main/java/nl/quintor/qodingchallenge/dto/AmountOfQuestionType.java
@@ -0,0 +1,52 @@
+package nl.quintor.qodingchallenge.dto;
+
+import java.util.Objects;
+
+public class AmountOfQuestionType {
+    public String type;
+    public int amount;
+
+    public AmountOfQuestionType(String type, int amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AmountOfQuestionType that = (AmountOfQuestionType) o;
+        return amount == that.amount &&
+                Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, amount);
+    }
+
+    @Override
+    public String toString() {
+        return "AmountOfQuestionType{" +
+                "type='" + type + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
